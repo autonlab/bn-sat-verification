1b7a7c406e9158623af9b7b98f507f1c1a64e219
Ignacy Stepka <ignacy.stepka@gmail.com>
2023-06-19 15:37:36 -0400

    Add simple verification queries to pytest tests, which aim is to ensure that encoding is correct in some basic cases

--- tests/test_basic_queries.py
+++ tests/test_basic_queries.py
@@ -10,10 +10,7 @@ print("Current working directory: ", os.getcwd())
 pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 src_path = os.path.join(pardir, "src")
 os.chdir(src_path)
-
 sys.path.insert(0, src_path)
-
-# print("Current working directory: ", os.getcwd())
 
 from typing import Dict, List, Tuple
 from pysat.formula import CNF
@@ -23,8 +20,6 @@ from src.odd_parser import read_obdd_from_file
 from src.pysat_solver import pysat_solver
 
 logging.basicConfig(level=logging.DEBUG)
-
-
 
 @pytest.fixture
 def get_data() -> Tuple[CNF, Dict, Dict]:
@@ -61,7 +56,7 @@ def convert_to_assumptions(to_set: Dict[str, int], mapping: Dict[str, int]) -> L
             assumptions.append(-int(mapping[var]))
     return assumptions
     
-class TestBasicQueries:
+class TestEncodingWithSimpleQueries:
     def test_if_any_clauses(self, get_data):
         # Access the variable set up by the fixture
         cnf, _, _ = get_data
@@ -118,3 +113,72 @@ class TestBasicQueries:
         
         should_SAT = pysat_solver(cnf, set_variables=assumpt)
         assert should_SAT, "Should be SAT"
+        
+    def test_if_consistent(self, get_data):      
+        cnf, mapping, _ = get_data
+
+        to_set = {
+            'Node_GPA_1': 1, # First node should be true - we have to set it to true
+            # This does not mean that we set the GPA to 1, but that we set the first node to true
+            'Node_TRUE_4': 1, # True node should be true
+            'Node_FALSE_5': 1, # False node should be false, but we set it to true
+            # This should make the diagram inconsistent
+        }
+
+        assumpt = convert_to_assumptions(to_set, mapping)
+
+        should_UNSAT = pysat_solver(cnf, set_variables=assumpt)
+        assert should_UNSAT == None, "Should be UNSAT, because the assumptions are inconsistent. \
+            But it is SAT = wrong!!."
+            
+    def test_fail_if_two_nodes_at_the_same_level_are_active(self, get_data):
+        cnf, mapping, _ = get_data
+          
+        to_set = {
+            'Node_WorkExperience_2': 1,
+            'Node_WorkExperience_3': 1,
+        }
+
+        assumpt = convert_to_assumptions(to_set, mapping)
+
+        model = pysat_solver(cnf, set_variables=assumpt)
+        
+        assert model == None, "Should be UNSAT"
+            
+        
+    def test_fail_if_two_edges_at_the_same_level_are_active(self, get_data): 
+        cnf, mapping, _ = get_data
+        
+        to_set = {
+            'edge_2_4': 1,
+            'edge_2_5': 1,
+        }
+                
+        assumpt =  convert_to_assumptions(to_set, mapping)
+
+        should_UNSAT = pysat_solver(cnf, set_variables=assumpt)
+        assert should_UNSAT == None, "Should be UNSAT, because the assumptions are inconsistent. \
+            But it is SAT = wrong!!."            
+
+    def test_if_at_most_one_path_is_active(self, get_data):
+        cnf, mapping, _ = get_data
+        
+        
+        # Go over all the outgoing edges of each of the nodes
+        # and make sure that at most one of them is active 
+        
+        for i in range(len([x for x in mapping.keys() if x.startswith('Node')]) - 2):
+            print(f"Testing if at most one path is active for node {i}")
+            to_set = {            
+                'Node_TRUE_4': 1, # True node should be true
+                'Node_FALSE_5': 1, # False node should be false
+            }
+            
+            for node in mapping.keys():
+                if node.startswith(f'edge_{i}'):
+                    to_set[node] = 1
+                
+            assumpt = convert_to_assumptions(to_set, mapping)
+
+            should_UNSAT = pysat_solver(cnf, set_variables=assumpt)
+            assert should_UNSAT is None, f"Should be SAT, found a counterexample: {should_UNSAT}"
