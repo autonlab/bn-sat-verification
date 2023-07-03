b9d35260e3a91217ecf54545142ff7586ad59a73
Ignacy Stepka <ignacy.stepka@gmail.com>
2023-07-03 13:51:13 -0400

    Add ensemble of experts encoding

--- src/run.py
+++ src/run.py
@@ -1,14 +1,23 @@
 import logging
 import os
-from scripts.convert_to_shih import convert_to_shih
+from typing import List
+
 from convert_net_to_odd import run_conversion
 from odd_parser import read_obdd_from_file
-from utils.draw_diagram import draw_obdd
 from tseitin_encoding import TseitinEncoder
+
+from scripts.convert_to_shih import convert_to_shih
+
+from utils.draw_diagram import draw_obdd
+from utils.ensemble_encoding import ensemble_encode
+from utils.cnf_json_parser import read_cnf_from_json, save_cnf_to_json
+
+from verifications.pysat_solver import PySATSolver
+from verifications.solver_class import SATSolver
 
 DATASET_NAME = "darpatriage"
 VARS = 11
-DATASET_ROOT = "Delayed"
+OUTCOMES = ["Immediate", "Delayed", "Minimal"]
 LEAVES = ["Breathless", 
             "EventType", 
             "PerfusionCondition", 
@@ -22,18 +31,18 @@ LEAVES = ["Breathless",
             "TorsoDetected"
             ]
 RESULTS_DIR = f"results/{DATASET_NAME}"
-
 DATASET_CONFIG = {
-    "id": DATASET_ROOT,
+    "id": None,
     "name": DATASET_NAME,
     "filetype": "net",
     "vars": 11,
-    "root": DATASET_ROOT,
+    "root": None,
     "leaves": LEAVES,
     "threshold": 0.5000001,
     "input_filepath": "../bnc_networks/",
     "output_filepath": "../odd_models/"
 }
+
 
 def run_xsdl_to_net(dataset_name: str) -> bool:
     '''
@@ -96,7 +105,36 @@ def run_encoding(odd_filename: str, cnf_save_filename: str) -> None:
     
     encoder.save_to_json(cnf_save_filename)
     
+def test_if_cnf_satisfiable(cnf_filename: str, solver: SATSolver = None) -> None:
+    '''
+    Runs the SAT solver to check if the CNF is satisfiable
+    '''
+    cnf, _, _, _ = read_cnf_from_json(cnf_filename)
 
+    if solver is None:
+        solver = PySATSolver()
+    
+    model = solver.solve(cnf)
+    if model is None:
+        logging.error(f"CNF is unsatisfiable")
+    else:
+        logging.info(f"CNF is satisfiable")
+        logging.debug(f"Model: {model}")
+        
+def run_encoding_ensemble(cnf_files: List[str], cnf_save_filename: str) -> None:
+    '''
+    Connect CNFs to form one large cnf
+    '''
+    cnf, inverse_map, mapping, map_variable_names = ensemble_encode(cnf_files)
+    
+    save_cnf_to_json(cnf_save_filename,
+                     cnf=cnf,
+                     map_inv=inverse_map,
+                     map=mapping,
+                     map_names_vars=map_variable_names
+    )
+    
+    
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s|%(levelname)s: %(message)s', datefmt="%Y-%m-%d|%H:%M:%S")
 
@@ -114,11 +152,24 @@ if __name__ == '__main__':
         
     run_xsdl_to_net(DATASET_NAME)
     
-    run_net_to_odd_compilation(DATASET_NAME, DATASET_CONFIG)
     
-    odd_filename = f"odd_models/{DATASET_NAME}_{DATASET_ROOT}.odd"
-    save_path = f"{RESULTS_DIR}/{DATASET_NAME}/{DATASET_NAME}_{DATASET_ROOT}.png"
-    run_plot_odd(odd_filename=odd_filename, save_path=save_path, display_block=False)
+    cnf_filenames = []
+    for DATASET_ROOT in OUTCOMES:
+        logging.info(f"RUNNING PIPELINE FOR {DATASET_NAME} WITH DATASET_ROOT: {DATASET_ROOT}")
+        DATASET_CONFIG["root"] = DATASET_ROOT
+        DATASET_CONFIG['id'] = DATASET_ROOT
+        
+        # run_net_to_odd_compilation(DATASET_NAME, DATASET_CONFIG)
     
-    cnf_filename = f"cnf_files/{DATASET_NAME}_{DATASET_ROOT}.cnf"
-    run_encoding(odd_filename=odd_filename, cnf_save_filename=cnf_filename)
+        odd_filename = f"odd_models/{DATASET_NAME}_{DATASET_ROOT}.odd"
+        save_path = f"{RESULTS_DIR}/{DATASET_NAME}/{DATASET_NAME}_{DATASET_ROOT}.png"
+        # run_plot_odd(odd_filename=odd_filename, save_path=save_path, display_block=False)
+    
+        cnf_filename = f"cnf_files/{DATASET_NAME}_{DATASET_ROOT}.json"
+        cnf_filenames.append(cnf_filename)
+        run_encoding(odd_filename=odd_filename, cnf_save_filename=cnf_filename)
+
+        test_if_cnf_satisfiable(cnf_filename=cnf_filename)
+    
+    run_encoding_ensemble(cnf_files=cnf_filenames, cnf_save_filename=f"cnf_files/{DATASET_NAME}_ensemble.json")
+    
---
+++ src/utils/ensemble_encoding.py
@@ -0,0 +1,151 @@
+from typing import List, Dict, Tuple
+from utils.cnf_json_parser import read_cnf_from_json, save_cnf_to_json
+from pysat.formula import CNF
+
+def merge_two_models(cnf1: List[List[int]], 
+                     cnf2: List[List[int]],
+                     map1: Dict[str, int],
+                     map2: Dict[str, int],
+                     inverse_map1: Dict[int, str],
+                     inverse_map2: Dict[int, str],
+                     map_names_vars1: Dict[str, List[str]],
+                     map_names_vars2: Dict[str, List[str]],
+                     model_index: int
+                     ) -> Tuple[List[List[int]], Dict[int, str], Dict[str, int], Dict[str, str]]:
+    '''
+    Merge two models into a single model by combining the variables.
+    All variables in both models that are the same will be combined in order to create a single, consistent model.
+    '''
+    
+    
+    common_indexer = 0
+    common_map = {}
+    common_map_names_vars = {}
+    
+    for _mapnames in [map_names_vars1, map_names_vars2]:
+        for vname, vvalueslist in _mapnames.items():
+            
+            if vname not in common_map_names_vars:
+                common_map_names_vars[vname] = set()
+            
+            for k in vvalueslist:
+                print(vvalueslist)
+                ith_value = k.split('=')[1]
+                name = f'{vname}={ith_value}'
+                if name not in common_map:
+                    common_map[name] = common_indexer
+                    common_indexer += 1
+                    
+                common_map_names_vars[vname].add(name)
+               
+    first = (cnf1, map1, inverse_map1)
+    second = (cnf2, map2, inverse_map2)
+    
+    
+    for m_index, _map in enumerate([map1, map2]):
+        for name in _map:
+            if name.startswith('x_') or '=' in name:
+                continue
+            else:
+                if m_index == 1:
+                    common_map[f'{name}__{model_index}'] = common_indexer
+                else:
+                    common_map[f'{name}'] = common_indexer
+                common_indexer += 1
+    
+    new_cnf = []
+    for m_index, (_cnf, _map, _inverse) in enumerate([first, second]):
+        for clause in _cnf:
+            new_clause = [] 
+            for literal in clause:
+                sign = -1 if literal < 0 else 1
+                literal_int_as_string = abs(literal)
+                literal_name = _inverse[literal_int_as_string]
+                
+                if literal_name.startswith('x_') or '=' in literal_name:
+                    ith_value = k.split('=')[1]
+                    name = f'{vname}={ith_value}'
+                else:
+                    if m_index == 1:
+                        name = f'{literal_name}__{model_index}'
+                    else:
+                        name = literal_name
+                    
+                new_literal = common_map[name] * sign
+                new_clause.append(new_literal)
+        
+            new_cnf += [new_clause]
+                
+    common_map_inverse = __create_inverse_map(common_map) 
+     
+    print(new_cnf)
+    print(model_index)
+    
+    return new_cnf, common_map_inverse, common_map, common_map_names_vars
+ 
+def __create_inverse_map(maping: Dict[str, int]) -> Dict[int, str]:
+    '''
+    Creates an inverse map from the given map
+    '''
+    inverse_map = {}
+    for key, value in maping.items():
+        inverse_map[value] = key
+
+    return inverse_map
+    
+
+def ensemble_encode(cnf_files: List[str]) -> Tuple[CNF, Dict[int, str], Dict[str, int], Dict[str, str]]:
+    '''
+    Encodes a list of CNF files to a single CNF file
+    
+    Args:
+        - cnf_files: A list of CNF files
+    
+    Returns:
+        - The CNF file
+        - The inverse maping of the variables
+        - The maping of the variables
+        - The maping of the variable names
+    '''
+    ensemble_cnf = []
+    ensemble_map = {}
+    ensemble_inverse_map = {}
+    ensemble_map_var_names = {}
+    
+    for idx, cnf_file in enumerate(cnf_files):
+        cnf, inverse_map, maping, map_var_names = read_cnf_from_json(cnf_file)
+        
+        inverse_map = {int(k):v for k,v in inverse_map.items()}
+        maping = {k:int(v) for k,v in maping.items()}
+        
+        
+        if not ensemble_cnf: 
+            ensemble_cnf = cnf.clauses
+            ensemble_map = maping
+            ensemble_inverse_map = inverse_map
+            ensemble_map_var_names = map_var_names
+        else:
+            ensemble_cnf, ensemble_inverse_map, ensemble_map, ensemble_map_var_names = merge_two_models(
+                                ensemble_cnf, 
+                                cnf.clauses, 
+                                ensemble_map, 
+                                maping, 
+                                ensemble_inverse_map,
+                                inverse_map,
+                                ensemble_map_var_names, 
+                                map_var_names,
+                                idx
+                                )
+        
+        
+    ensemble_inverse_map = __create_inverse_map(ensemble_map)
+    
+    _cnf = CNF()
+    for clause in ensemble_cnf:
+        _cnf.append(clause)
+    
+    return _cnf, ensemble_inverse_map, ensemble_map, ensemble_map_var_names
+        
+    
+        
+        
