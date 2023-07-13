8f2262bde3b575ab9e8f087514a2ce2be4b50b1a
Ignacy Stepka <ignacy.stepka@gmail.com>
2023-07-13 10:05:48 -0400

    Make monotonicity verification work

--- src/verifications/verification_multiclass_monotonicity.py
+++ src/verifications/verification_multiclass_monotonicity.py
@@ -29,7 +29,7 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             self.sink_names_in_order = sink_names_in_order
             self.sinks_count = len(self.sink_names_in_order)
             self.variable_to_verify = variable_to_verify
-            self.variable_to_verify_index = self.map[variable_to_verify]
+            # self.variable_to_verify_index = self.map[variable_to_verify]
             self.map_name_vars = map_name_vars
             
             
@@ -48,35 +48,44 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             for clause in cnf.clauses:
                 for lit in clause:
                     max_var = max(max_var, abs(lit))
-                        
-            altered_cnf = strip_sinks(cnf=cnf, sinks_map=self.sink_names_in_order, mapping=self.map).clauses  
-            
-            models = [deepcopy(altered_cnf.clauses) for _ in range(3)]
+               
+            altered_cnf = strip_sinks(cnf=cnf, sinks_map=self.sink_names_in_order, mapping=self.map)  
+                 
+            models = []
+            for i in range(3):
+                models.append(deepcopy(altered_cnf))
             offsets = [0, max_var, 2 * max_var]
-            
+                    
             # Translate models by offsets
             for i in range(3):
                 for clause in models[i]:
                     for j in range(len(clause)):
-                        clause[j] += offsets[i]
+                        if clause[j] < 0:
+                            clause[j] -= offsets[i]
+                        else:
+                            clause[j] += offsets[i]
             
-            variable_values = [self.map[name] for name in self.map_name_vars[self.variable_to_verify]]
+            variable_values = [int(self.map[name]) for name in self.map_name_vars[self.variable_to_verify]]
             
+            if len(variable_values) < 3:
+                raise ValueError(f'Variable {self.variable_to_verify} has {len(variable_values)} values. It should have at least 3 values in order to run this verification task.')
             
             # --- VAR_ORD --- #
             # X2 > X1
-            VAR_ORD, max_var = A_greater_than_B(a=[a + offsets[1] for a in variable_values], 
+            VAR_ORD1, max_var = A_greater_than_B(a=[a + offsets[1] for a in variable_values], 
                              b=variable_values, 
                              max_var=3 * max_var) 
             # X3 > X2
-            VAR_ORD, max_var += A_greater_than_B(a=[a + offsets[2] for a in variable_values],
+            VAR_ORD2, max_var = A_greater_than_B(a=[a + offsets[2] for a in variable_values],
                                 b=[a + offsets[1] for a in variable_values], 
-                                max_var=3 * max_var)
+                                max_var=max_var)
             # X3 > X1
-            VAR_ORD, max_var += A_greater_than_B(a=[a + offsets[2] for a in variable_values],
+            VAR_ORD3, max_var = A_greater_than_B(a=[a + offsets[2] for a in variable_values],
                                 b=variable_values, 
-                                max_var=3 * max_var)
+                                max_var=max_var)
+            VAR_ORD = VAR_ORD1 + VAR_ORD2 + VAR_ORD3
             
+            print(f'MAX VAR = {max_var}')
             
             LHL = []
             HLH = []
@@ -84,30 +93,33 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             true_sinks = []
             for sinks in self.sink_names_in_order:
                 if 'TRUE' in sinks[0]:
-                    true_sinks.append(self.map[sinks[0]])
+                    true_sinks.append(int(self.map[sinks[0]]))
                 else:
-                    true_sinks.append(self.map[sinks[1]])
+                    true_sinks.append(int(self.map[sinks[1]]))
             
             # --- LHL --- # 
             # Y2 > Y1
-            LHL, max_var += A_greater_than_B(a=[a + offsets[1] for a in true_sinks], 
+            LHL1, max_var = A_greater_than_B(a=[a + offsets[1] for a in true_sinks], 
                              b=true_sinks, 
-                             max_var=3 * max_var)
+                             max_var=max_var)
             # Y2 > Y3
-            LHL, max_var += A_greater_than_B(a=[a + offsets[1] for a in true_sinks],
+            LHL2, max_var = A_greater_than_B(a=[a + offsets[1] for a in true_sinks],
                                 b=[a + offsets[2] for a in true_sinks], 
-                                max_var=3 * max_var)
+                                max_var=max_var)
+            LHL = LHL1 + LHL2
             
             # --- HLH --- #
             # Y2 < Y1
-            HLH, max_var += A_greater_than_B(a=true_sinks,
+            HLH1, max_var = A_greater_than_B(a=true_sinks,
                                 b=[a + offsets[1] for a in true_sinks], 
-                                max_var=3 * max_var)
+                                max_var=max_var)
             # Y2 < Y3
-            HLH, max_var += A_greater_than_B(a=[a + offsets[2] for a in true_sinks],
+            HLH2, max_var = A_greater_than_B(a=[a + offsets[2] for a in true_sinks],
                                 b=[a + offsets[1] for a in true_sinks], 
-                                max_var=3 * max_var)
-            
+                                max_var=max_var)
+                   
+            print(f'MAX VAR = {max_var}')
+            HLH = HLH1 + HLH2
 
             # Create two separate verification tasks for LHL and HLH.
             verif1 = models[0] + models[1] + models[2] + VAR_ORD + LHL
@@ -116,13 +128,12 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             verif2 = models[0] + models[1] + models[2] + VAR_ORD + HLH
             verif2 = deepcopy(verif2)
             
-            
             outcome1 = sat_solver.solve(cnf=CNF(from_clauses=verif1))
             outcome2 = sat_solver.solve(cnf=CNF(from_clauses=verif2))
             
             if outcome1 is None and outcome2 is None:
                 logging.debug(f'Verification case #{self.name} is UNSAT.')
-                logging.debug(f'Verification case #{self.name} model: {self.sat_model}')
+                logging.debug(f'Verification case #{self.name} model: {None}')
                 self.set_result(True)
                 return True
             
@@ -136,4 +147,6 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                     logging.debug(f'Verification case #{self.name} HLH model: {self.sat_model}')
                 self.set_result(False)
                 return False
+            
+            raise Exception('Unexpected outcome of the verification task.')
                 
