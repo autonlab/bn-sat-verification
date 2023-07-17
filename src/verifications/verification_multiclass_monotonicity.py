a16efc0b44de7bde6495c984050cb851a9d90d1b
Ignacy Stepka <ignacy.stepka@gmail.com>
2023-07-17 17:04:01 -0400

    Fix monotonicity verification by adding variable values sorting

--- src/verifications/verification_multiclass_monotonicity.py
+++ src/verifications/verification_multiclass_monotonicity.py
@@ -13,7 +13,8 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                         map: Dict[str, int], 
                         sink_names_in_order: List[Tuple[str, str]],
                         variable_to_verify: str,
-                        map_name_vars: Dict[str, List[str]]
+                        map_name_vars: Dict[str, List[str]],
+                        binary: bool = False
                    ) -> None:
             '''
             `map`: Mapping from variable names to their values. 
@@ -31,6 +32,7 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             self.variable_to_verify = variable_to_verify
             # self.variable_to_verify_index = self.map[variable_to_verify]
             self.map_name_vars = map_name_vars
+            self.binary = binary
             
             
         def verify(self, cnf: CNF, 
@@ -55,6 +57,8 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             for i in range(3):
                 models.append(deepcopy(altered_cnf))
             offsets = [0, max_var, 2 * max_var]
+            model_max_var = max_var + 2 * max_var
+            logging.debug(f'Model max var: {model_max_var}')
                     
             # Translate models by offsets
             for i in range(3):
@@ -65,10 +69,19 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                         else:
                             clause[j] += offsets[i]
             
-            variable_values = [int(self.map[name]) for name in self.map_name_vars[self.variable_to_verify]]
+            # variable_values = [int(self.map[name]) for name in self.map_name_vars[self.variable_to_verify]]
+
+            names = sorted(
+                [name for name in self.map_name_vars[self.variable_to_verify]], 
+                key=lambda x: int(x.split('=')[1].split('th')[0])
+                )
+            variable_values = [int(self.map[name]) for name in names]
+                
             
             if len(variable_values) < 3:
                 raise ValueError(f'Variable {self.variable_to_verify} has {len(variable_values)} values. It should have at least 3 values in order to run this verification task.')
+            logging.debug(f'Variable {self.variable_to_verify} has {len(variable_values)} values: {variable_values}.')
+            
             
             # --- VAR_ORD --- #
             # X2 > X1
@@ -85,17 +98,29 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                                 max_var=max_var)
             VAR_ORD = VAR_ORD1 + VAR_ORD2 + VAR_ORD3
             
-            print(f'MAX VAR = {max_var}')
+            logging.debug(f'MAX VAR = {max_var}')
             
             LHL = []
             HLH = []
-            
             true_sinks = []
-            for sinks in self.sink_names_in_order:
-                if 'TRUE' in sinks[0]:
-                    true_sinks.append(int(self.map[sinks[0]]))
+            
+            if not self.binary:
+                for sinks in self.sink_names_in_order:
+                    if 'TRUE' in sinks[0]:
+                        true_sinks.append(int(self.map[sinks[0]]))
+                    else:
+                        true_sinks.append(int(self.map[sinks[1]]))
+            else:
+                _s = self.sink_names_in_order[0]
+                if 'TRUE' in _s[0]: 
+                    true_sinks.append(int(self.map[_s[0]]))
+                    true_sinks.append(int(self.map[_s[1]]))
                 else:
-                    true_sinks.append(int(self.map[sinks[1]]))
+                    true_sinks.append(int(self.map[_s[1]]))
+                    true_sinks.append(int(self.map[_s[0]]))
+                                    
+            logging.debug(f'True sinks: {true_sinks}')
+            
             
             # --- LHL --- # 
             # Y2 > Y1
@@ -108,6 +133,8 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                                 max_var=max_var)
             LHL = LHL1 + LHL2
             
+            logging.debug(f'LHL: {LHL}')
+            
             # --- HLH --- #
             # Y2 < Y1
             HLH1, max_var = A_greater_than_B(a=true_sinks,
@@ -117,9 +144,16 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             HLH2, max_var = A_greater_than_B(a=[a + offsets[2] for a in true_sinks],
                                 b=[a + offsets[1] for a in true_sinks], 
                                 max_var=max_var)
+            
                    
-            print(f'MAX VAR = {max_var}')
+            logging.debug(f'MAX VAR = {max_var}')
             HLH = HLH1 + HLH2
+            logging.debug(f'HLH: {HLH}')
+            
+            # Check in there is any 0 in the CNF
+            for clause in models[0] + models[1] + models[2] + VAR_ORD + LHL + HLH:
+                if 0 in clause or '0' in clause:
+                    raise ValueError('CNF contains 0.')
 
             # Create two separate verification tasks for LHL and HLH.
             verif1 = models[0] + models[1] + models[2] + VAR_ORD + LHL
@@ -130,6 +164,7 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
             
             outcome1 = sat_solver.solve(cnf=CNF(from_clauses=verif1))
             outcome2 = sat_solver.solve(cnf=CNF(from_clauses=verif2))
+            
             
             if outcome1 is None and outcome2 is None:
                 logging.debug(f'Verification case #{self.name} is UNSAT.')
@@ -146,6 +181,21 @@ class VerificationCaseMulticlassMonotonicity(VerificationCase):
                     self.sat_model = outcome2
                     logging.debug(f'Verification case #{self.name} HLH model: {self.sat_model}')
                 self.set_result(False)
+                
+                inv_map = {v: k for k, v in self.map.items()}
+                for v in self.sat_model:
+                    if v > 0: 
+                        if v < offsets[1]:
+                            v = v
+                            logging.debug(f' [1] Variable {inv_map[str(v)]} is true.')
+                        if v >= offsets[1] and v < offsets[2]:
+                            v = v - offsets[1] 
+                            logging.debug(f' [2] Variable {inv_map[str(v)]} is true.')
+                        if v >= offsets[2] and v < model_max_var:
+                            v = v - offsets[2]
+                            logging.debug(f' [3] Variable {inv_map[str(v)]} is true.')
+                        
+                
                 return False
             
             raise Exception('Unexpected outcome of the verification task.')
